24a840ac8bddd6c0c705358873437237fdd6e1d7
yeonwo00 <yeonwoo040323@gmail.com>
2026-07-21 04:21:48 +0900

    Update sum-nbo.cpp

--- sum-nbo.cpp
+++ sum-nbo.cpp
@@ -3,40 +3,47 @@
 #include <arpa/inet.h>
 
 uint32_t read(const char* filename) {
-    FILE* fp = fopen(filename, "rb");
 
-    if (fp == NULL) {
-        perror(filename);
-        return 0;
-    }
+        FILE *fp = fopen(filename, "rb");
 
-    uint32_t n_number;
+        if (fp == NULL) {
+                perror(filename);
+                return 0;
+        }
 
-    if (fread(&n_number, sizeof(n_number), 1, fp) != 1) {
-        fprintf(stderr, "%s : can't read file\n", filename);
+        uint32_t n_number;
+
+        if (fread(&n_number, sizeof(n_number), 1, fp) != 1) {
+                fprintf(stderr, "%s : can't read file\n", filename);
+                fclose(fp);
+                return 0;
+        }
+
+
         fclose(fp);
-        return 0;
-    }
 
-    fclose(fp);
-    return ntohl(n_number);
+        return ntohl(n_number); 
+
 }
 
-int main(int argc, char* argv[]) {
-    uint32_t number;
-    uint32_t sum = 0;
 
-    for (int i = 1; i < argc; i++) {
-        number = read(argv[i]);
-        printf("%u(0x%08x)", number, number);
-        sum += number;
+int main(int argc, char *argv[]) {
 
-        if (i == argc - 1)
-            printf(" = %u(0x%08x)", sum, sum);
-        else
-            printf(" + ");
-    }
+        uint32_t number;
+        uint32_t sum = 0;
 
-    printf("\n");
-    return 0;
+        for (int i = 1; i < argc; i++) {
+                number = read(argv[i]);
+
+                printf("%u(0x%08x)", number, number);
+
+                sum += number;
+
+                if (i == (argc - 1))
+                        printf(" = %u(0x%08x)", sum, sum);
+                else
+                        printf(" + ");
+        }
+
+        return 0;
 }
